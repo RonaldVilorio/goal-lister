73079e85c135e6f394d9199e1a69f11c5bbdd12e
ronald vilorio <soccer23415@gmail.com>
2018-03-13 20:22:40 +0000

    added controller files

--- app/models/subgoal.rb
+++ app/models/subgoal.rb
@@ -1,5 +1,5 @@
 class Subgoal < ActiveRecord::Base
   has_many :goal_subgoals
-  has many :goals, through: :goal_subgoals
+  has_many :goals, through: :goal_subgoals
   has_many :users, through: :goals
 end
