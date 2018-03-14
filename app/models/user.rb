f44236e4eb0af6c1a2e40e9daf47b02b79321e2d
ronald vilorio <soccer23415@gmail.com>
2018-03-14 01:03:05 +0000

    added slugify file/started on views files

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
-has_many :goals
-has_many :subgoals, through: :goals
+  include Concerns::Slugify
+  extend Concerns::Findable
+  has_many :goals
+  has_many :subgoals, through: :goals
 end
