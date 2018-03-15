d93455a0f443af42620b36b57eafeffc3ea13a94
ronald vilorio <soccer23415@gmail.com>
2018-03-15 15:30:45 +0000

    adjusted javascript code to deliver desired params

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -6,10 +6,13 @@ class GoalsController < ApplicationController
   get '/goals/new' do
     erb :'/goals/create_goal'
   end
-  # implement a goals/slug route on next line
+  # implement a goals/slug route on next line ?
   get '/goals/:id' do
     @goal = Goal.find_by_slug(params[:slug])
     erb :'/goals/show_goal'
+  end
+  post '/goals' do
+    binding.pry
   end
 
 end
