42776d40d27eda51b9e1e5809ea7144a81eab60b
ronald vilorio <soccer23415@gmail.com>
2018-03-17 01:21:48 +0000

    added routes for subgoals controller

--- app/controllers/subgoals_controller.rb
+++ app/controllers/subgoals_controller.rb
@@ -1,2 +1,18 @@
 class SubgoalsController < ApplicationController
+  get '/subgoals' do
+    if !logged_in?
+      redirect to :'/login'
+    else
+      erb :'/subgoals/subgoals'
+    end
+  end
+  get '/subgoals/new' do
+    if !logged_in?
+      redirect to :'/login'
+    else
+      erb :'/subgoals/create_subgoal'
+    end
+
+  end
+
 end
