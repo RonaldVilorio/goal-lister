cc3014605907aab4edd7e6d8d9e1533bcfa98f47
ronald vilorio <soccer23415@gmail.com>
2018-03-17 14:40:22 +0000

    took away create subgoal view and controller/subgoals view progress

--- app/controllers/subgoals_controller.rb
+++ app/controllers/subgoals_controller.rb
@@ -6,13 +6,5 @@ class SubgoalsController < ApplicationController
       erb :'/subgoals/subgoals'
     end
   end
-  get '/subgoals/new' do
-    if !logged_in?
-      redirect to :'/login'
-    else
-      erb :'/subgoals/create_subgoal'
-    end
-
-  end
-
+  
 end
